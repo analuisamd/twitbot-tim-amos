1fed4dc4d77c49d6bcf6ce194428eb552a826cdc
ana luisa <55565972+analuisamd@users.noreply.github.com>
2022-11-17 09:17:39 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,51 @@
+# twitbot-tih
+
+A Twitter bot that tweets a message of love everyday
+
+## Dependencies
+
+- Python 3, and modules:
+
+  twython,
+  pyOpenSSL,
+  ndg-httpsclient,
+  pyasn1
+
+- Unix/Linux environment and the 'date' command
+
+- If you run on a Raspberry Pi, you might need to install `rng-tools` for the
+  SSL connection to work
+
+## Usage
+
+1. Install with the following commands
+
+    ```bash
+    git clone https://github.com/analuisamd/twitbot-tim-amos.git 
+    cd twitbot-tim-amos
+    make install
+    ```
+
+2. Create a Twitter app and obtain the API Key, API Secret, Access
+Token, and Access Token Secret for the app. This can be done by
+following the instructions at:
+http://www.instructables.com/id/Raspberry-Pi-Twitterbot/?ALLSTEPS.
+
+3. Override the corresponding strings in the file '.auth' with
+appropriate Twitter app API access token strings obtained in the last
+step.
+
+4. Activate the virtual environment.
+
+    ```bash
+    . ./venv
+    ```
+
+5. In virtual environment, run 'python tim_amos.py' to tweet.
+
+6. (Optional) Create a cron job to invoke the bot once a day.
+[twitbot-tih-run.sh](twitbot-tih-run.sh) shows an example script that can be
+called within the cronjob.
+
+7. To uninstall, `deactivate` from the virtual environment if needed, and do
+`make clean`.
---
+++ tim_amos_bot.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import random
+import subprocess
+from twython import Twython
+
+
+ApiKey = ''
+ApiSecret = ''
+AccessToken = ''
+AccessTokenSecret = ''
+cred_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                         '.auth')
+twitter_allowed_char = 260
+
+
+def get_api_token():
+    ''' Obtain Twitter app's API token from file .auth
+
+    Returns list
+    '''
+    with open(cred_file, 'rb') as f:
+        c = f.read()
+        t = c.splitlines()
+        return t[0:4]
+
+
+def get_today_str():
+    ''' Obtain current date in 'Month_Date' format, e.g., March 3
+
+    Returns str
+    '''
+    d = subprocess.check_output(["date", "+%B_%d"])
+    d_str = d.strip()
+    return d_str.decode('utf-8')
+
+
+def get_tweet_str():
+    ''' Obtain the string to tweet. It does sanity checks.
+
+    Returns str
+    '''
+    lilis = " @91bowie"
+    today_str = get_today_str()
+    tweet_str = 'Nothing to tweet today. #' + today_str
+    
+    tim_amos_list = ["tim amos lilis", "i know nobody understands me like you do", "You know, when I'm with you I'm so much happier", "tim amos liliquinhas"] 
+    list_size = len(tim_amos_list)
+    if list_size == 0:
+        return tweet_str
+
+    else:
+        i = random.randrange(0, list_size)
+        entry_str = tim_amos_list[i]
+        tweet_str = entry_str + lilis
+
+    return tweet_str
+
+
+def do_tweet(str):
+    ''' Tweet str to Twitter
+
+    '''
+    [ApiKey, ApiSecret, AccessToken, AccessTokenSecret] = get_api_token()
+    api = Twython(ApiKey, ApiSecret, AccessToken, AccessTokenSecret)
+    api.update_status(status=str)
+    print("Tweeted: ", str)
+
+
+if __name__ == '__main__':
+    tweet_str = get_tweet_str()
+    print(tweet_str)
+    do_tweet(tweet_str)
